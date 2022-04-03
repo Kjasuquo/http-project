96267e0ad0d5ee6d236f3feab8448b46463f1208
Kjasuquo <joseph.asuquo@decagon.dev>
2022-04-03 07:57:36 +0100

    race conditions handled and testing stated

--- myBlog/functions.go
+++ myBlog/functions.go
@@ -20,15 +20,10 @@ type Data struct {
 	Title   string
 	Content string
 	Status  bool
-	Edit    string
-	Delete  string
 }
 
 //DataStructure is a database in which my data are stored both before and after manipulation
 var DataStructure []Data
-
-//For holding data to be edited
-var d Data
 
 //Register initializes all my commands and html pages and it is called in the main
 func Register(router *chi.Mux) {
@@ -55,12 +50,12 @@ func indexhandler(w http.ResponseWriter, r *http.Request) {
 //To get content page
 func getContenthandler(w http.ResponseWriter, r *http.Request) {
 	//This points to the html location
-	// /post?url=12345
+
 	t, e := template.ParseFiles("templat/addpost.html")
 	Error(e)
 
 	//This displays whatever on the html page
-	e = t.ExecuteTemplate(w, "addpost.html", nil)
+	e = t.Execute(w, nil)
 	Error(e)
 }
 
@@ -81,14 +76,10 @@ func postContenthandler(w http.ResponseWriter, r *http.Request) {
 	f.Title = title
 	f.Content = content
 	f.Status = true
-	f.Edit = "Edit"
-	f.Delete = "Delete"
 	f.Id = uuid.NewString() //new id is being populated for an item using google/uuid
 
-	if f.Content != "" {
-		//Attach the f to the Data base so that it can be populated on the index page
-		DataStructure = append(DataStructure, f)
-	}
+	//Attach the f to the Data base so that it can be populated on the index page
+	DataStructure = append(DataStructure, f)
 
 	//redirect your page back to the index/home page when done (on a click)
 	http.Redirect(w, r, "/", 302)
@@ -115,6 +106,7 @@ func deleteByIdhandler(w http.ResponseWriter, r *http.Request) {
 //To get the content on a page when Edit is clicked
 func updateByIdhandler(w http.ResponseWriter, r *http.Request) {
 	//Whenever it is clicked, get the id of its element
+	var d Data
 	ID := chi.URLParam(r, "Id")
 
 	//Range through the database comparing the gotten id and perform basic algorithm on it
@@ -123,22 +115,22 @@ func updateByIdhandler(w http.ResponseWriter, r *http.Request) {
 
 			//This stores the data gotten in d for easy manipulation/editting
 			d = DataStructure[i]
-
-			//This points to the html location
-			t, e := template.ParseFiles("templat/editpost.html")
-			Error(e)
-
-			//Calls or writes the item inside that database in the html file/template where it is called
-			e = t.Execute(w, DataStructure[i])
-			Error(e)
-
+			break
 		}
-	}
+	} //This points to the html location
+	t, e := template.ParseFiles("templat/editpost.html")
+	Error(e)
+
+	//Calls or writes the item inside that database in the html file/template where it is called
+	e = t.Execute(w, d)
+	Error(e)
 }
 
 //After getting the content to be edited in html page, after editing, it will create a new item of it and store in database
 func postupdateByIdhandler(w http.ResponseWriter, r *http.Request) {
 	//This gets/populates the content of the form
+	//var d Data
+	id := chi.URLParam(r, "Id")
 	e := r.ParseForm()
 	Error(e)
 
@@ -146,22 +138,17 @@ func postupdateByIdhandler(w http.ResponseWriter, r *http.Request) {
 	title := r.PostForm.Get("tit")
 	content := r.PostForm.Get("con")
 
-	//Filling the data struct instance
-	d.Title = title
-	d.Content = content
-
-	if d.Content != "" {
-
-		//This is to check the DataStructure for id matching that of data d.id and delete
-		for i, _ := range DataStructure {
-			if DataStructure[i].Id == d.Id {
-				DataStructure = append(DataStructure[:i], DataStructure[i+1:]...)
-			}
+	//This is to check the DataStructure for id matching that of data d.id and delete
+	for i, _ := range DataStructure {
+		if DataStructure[i].Id == id {
+			//DataStructure = append(DataStructure[:i], DataStructure[i+1:]...)
+			DataStructure[i].Title = title
+			DataStructure[i].Content = content
 		}
-
-		//Attach the f to the Data base so that it can be populated on the index page
-		DataStructure = append(DataStructure, d)
 	}
+
+	//Attach the f to the Data base so that it can be populated on the index page
+	//DataStructure = append(DataStructure, d)
 
 	//redirect your page back to the index/home page when done (on a click)
 	http.Redirect(w, r, "/", 302)
--- myBlog/functions_test.go
+++ myBlog/functions_test.go
@@ -2,44 +2,48 @@ package myBlog
 
 import (
 	"github.com/go-chi/chi"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
-
-func TestRegister(t *testing.T) {
-	r:= chi.NewRouter()
+func TestApplication_Indexhandle(t *testing.T) {
+	r := chi.NewRouter()
 	Register(r)
-	req, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil{
-		return
-	}
-	resp:= httptest.NewRecorder()
-	r.ServeHTTP(resp, req)
+	testServer := httptest.NewServer(r)
 
-	var tests = []struct{
-		
+	req, err := http.NewRequest(http.MethodGet, testServer.URL+"/", nil)
+	if err != nil {
+		t.Errorf("%v", err)
 	}
+	//resp := httptest.NewRecorder()
+	//indexhandler(resp, req)
+
+	resp, err := http.DefaultClient.Do(req)
+	log.Println(resp)
 }
 
-func TestError(t *testing.T) {
-	//router := chi.NewRouter()
-	//
-	//req, err := http.NewRequest(http.MethodGet, "/", nil)
-	//resp := httptest.NewRecorder()
-	//router.ServeHTTP(resp, req)
-	var tests = []struct {
-		input string
-		want  string
-	}{
-		{"No Error", "No Error"},
+func TestApplication_PostContenthandler(t *testing.T) {
+	r := chi.NewRouter()
+	Register(r)
+	testServer := httptest.NewServer(r)
+	form := url.Values{}
+	form.Add("title", "Active")
+	form.Add("content", "Active CSS")
+
+	req, err := http.NewRequest(http.MethodPost, testServer.URL+"/addpost", nil)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	req.Form = form
+	resp := httptest.NewRecorder()
+	postContenthandler(resp, req)
+	//log.Println(resp.Body)
+
+	if resp.Code != http.StatusFound {
+		t.Errorf("expected %v but got %v", http.StatusFound, resp.Code)
 	}
 
-	for _, test := range tests {
-		got := Error(test.input)
-		if got != test.want {
-			t.Errorf("expected %v but got %v", test.want, got)
-		}
-	}
 }
